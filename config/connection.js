d3cdf8e904e1bf34a68f5aa1938ad2bd3e11cb6c
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-02-15 14:51:31 -0700

    Modified connection.js for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Require mysql module
 var mysql = require("mysql");
+var connection;
 
 // Create connection to MySql database
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 // Start the database connection
 connection.connect(function(err) {
